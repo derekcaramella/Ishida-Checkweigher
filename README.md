a6c24cc47f4930518ce4a691a989f49e18328084
Derek Caramella <derekcaramella@gmail.com>
2021-03-24 07:41:20 -0400

    Updated the README.md file for increased documentation. Included database connectivity in the main.py file.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# ishida-checkweigher
+# Ishida Checkweigher
 DACS-G Ishida Checkweigher can send data through Modbus and RS-232 connections. The script is intended to be utilized on a Raspberry Pi, which can communicate with RS-232.
+
+### Capital Requirements:
+<li>Raspberry Pi 2 or greater generation</li>
+<li>Male USB to Male RS-232 port</li>
+<li>Female to Female RS-232 gender converter</li>
+<li>Female/Male Null modem adapter</li>
+
+### Scheduling Scrapping Script
+Cron is a tool for configuring scheduled tasks on Unix systems. Find cron <a href='https://www.raspberrypi.org/documentation/linux/usage/cron.md'> documentation</a> for implementation.
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import serial
 import time
+import pyodbc
+import settings
+
+con = pyodbc.connect(Trusted_Connection='no',
+                     driver='{SQL Server}',
+                     server=settings.database_ip,
+                     database='Alpha_Live',
+                     UID=settings.database_id,
+                     PWD=settings.database_password)
+cursor = con.cursor()
 
 workstation = 'Sig 1'  # Identify workstation.
 
-# Ensure baudrate, parity, stopbits, and byteszie are congruent with the Ishida data transfer protocool.
+# Ensure baud rate, parity, stop bits, and byte size are congruent with the Ishida data transfer protocol.
 ser = serial.Serial(port='/dev/ttyUSB0',
                     baudrate=2400,
                     parity=serial.PARITY_EVEN,
@@ -23,16 +33,18 @@ weight_status_dic = {'1': 'Proper', '2': 'Under', '3': 'Over', '4': 'Metal',
 # Read status output for five seconds, which should account for various line speeds & utilization.
 second = 0
 while second <= 5:
-    line= ser.readline()  # Read RS-232 output.
+    line = ser.readline()  # Read RS-232 output.
     line = str(line.decode('utf-8'))  # Decodes string output.
-    if len(line) >= 12:  # If ouput is greater than 13 characters long.
+    if len(line) >= 12:  # If output is greater than 13 characters long.
         command = line[0:2]  # First two characters.
         weight = float(line[2:10].lstrip('0'))  # Removes zero padding and converts to a float.
         unit_of_measure = line[10:11]  # Unit of Measure.
-        checksum = line[11:13]  #IDK
+        # Checksum: A digit representing the sum of the correct digits in a piece of stored or transmitted digital data.
+        check_sum = line[11:13]
         second += 5  # Exit loop
-        database_tuple = (weight_status_dic[command[1]], weight, unit_of_measure, weight_status_dic[checksum[0]], weight_status_dic[checksum[1]])  # Construct instance.
-        print(database_tuple)
+        database_tuple = (weight_status_dic[command[1]], weight, unit_of_measure, check_sum)  # Construct instance.
+        cursor.execute('USE [Alpha_Live] INSERT INTO [dbo].[Ishida Weights] VALUES ' + str(database_tuple))
+        con.commit()
     else:
         time.sleep(1)  # Wait one second before trying again.
         second += 1  # Loop through to acquire an instance.
